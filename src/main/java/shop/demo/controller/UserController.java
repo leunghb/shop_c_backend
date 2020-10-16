fbd1e6261832cada72fa52b50cc2d739f42a72e6
lhb <891547988@qq.com>
2020-10-17 06:55:56 +0800

    获取所有用户 getAllUser

--- src/main/java/shop/demo/controller/UserController.java
+++ src/main/java/shop/demo/controller/UserController.java
@@ -1,12 +1,17 @@
 package shop.demo.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.sun.xml.internal.ws.developer.Serialization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.demo.entity.Result;
 import shop.demo.entity.User;
 import shop.demo.service.UserService;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -14,9 +19,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /*获取所有用户 账号、创建时间*/
     @GetMapping("/getAllUser")
-    public List<User> getAllUser() {
-        System.out.println("======");
-        return userService.getAllUser();
+    public Result<Object> getAllUser() {
+        List<User> list = userService.getAllUser();
+        for (User user : list) {
+            System.out.println(user);
+            User user1 = new User();
+            user1.setAccount(user.getAccount());
+            user1.setCreatedAt(user.getCreatedAt());
+        }
+        return Result.success(user1);
     }
 }
---
+++ src/main/java/shop/demo/entity/Result.java
@@ -0,0 +1,87 @@
+package shop.demo.entity;
+
+public class Result<T> {
+    private String message;
+    private int code;
+    private T data;
+
+    private Result(T data) {
+        this.code = 0;
+        this.message = "成功";
+        this.data = data;
+    }
+
+    private Result(CodeMsg cm) {
+        if (cm == null) {
+            return;
+        }
+        this.code = cm.getCode();
+        this.message = cm.getMessage();
+    }
+
+    /**
+     * 成功时候的调用
+     *
+     * @return
+     */
+    public static <T> Result<T> success(T data) {
+        return new Result<T>(data);
+    }
+
+    public static <T> Result<T> success(CodeMsg cm) {
+        cm.setMessage(cm.getMessage());
+        return new Result<T>(cm);
+    }
+
+    public static <T> Result<T> success(CodeMsg cm, String msg) {
+        String str = (String) cm.getMessage();
+        str = str.indexOf("-") > 0 ? str.substring(0, str.indexOf("-")) : str;
+        cm.setMessage(str + "," + msg);
+        return new Result<T>(cm);
+    }
+
+    /**
+     * 成功，不需要传入参数
+     *
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Result<T> success() {
+        return (Result<T>) success("");
+    }
+
+    /**
+     * 失败时候的调用
+     *
+     * @return
+     */
+    public static <T> Result<T> error(CodeMsg cm) {
+        return new Result<T>(cm);
+    }
+
+    /**
+     * 失败时候的调用,扩展消息参数
+     *
+     * @param cm
+     * @param msg
+     * @return
+     */
+    public static <T> Result<T> error(CodeMsg cm, String msg) {
+        String str = (String) cm.getMessage();
+        str = str.indexOf("-") > 0 ? str.substring(0, str.indexOf("-")) : str;
+        cm.setMessage(str + "-" + msg);
+        return new Result<T>(cm);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
